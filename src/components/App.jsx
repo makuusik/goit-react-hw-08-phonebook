7dc75099bddffba100454efe5f96fd8d3afa7fa5
makuusik <makunka44@gmail.com>
2023-10-26 00:36:51 +0200

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,15 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-// import { setFilter } from './redux/contactsSlice';
 import ContactForm from './Contact/ContactForm';
 import ContactList from './Contact/ContactList';
 import Filter from './Filter/Filter';
 
 const App = () => {
-  // const dispatch = useDispatch();
   const contacts = useSelector(state => state.contacts.contacts);
-  // const filter = useSelector(state => state.contacts.filter);
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
-
-  // const handleFilterChange = value => {
-  //   dispatch(setFilter(value));
-  // };
 
   return (
     <div>
